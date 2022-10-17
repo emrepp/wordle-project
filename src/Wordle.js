6db2ab20ea731713c88ccb435e7026d90ba4ee77
Emma Repp <emmarepp.draws@gmail.com>
2022-10-16 19:34:59 -1000

    modal when correct

--- src/Wordle.js
+++ src/Wordle.js
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from './useWordle'
 import Grid from './Grid'
+import Modal from './Modal'
 
 export default function Wordle({solution}) {
     const { currentGuess, guesses, turn, isCorrect, handleKeyup } = useWordle(solution)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
      window.addEventListener('keyup', handleKeyup)  
+
+     if (isCorrect) {
+      console.log('won')
+      setTimeout(() => setShowModal(true), 2000)
+      window.removeEventListener('keyup', handleKeyup)
+     }
+
+     if (turn >5){
+      setTimeout(() => setShowModal(true), 2000)
+      window.removeEventListener('keyup', handleKeyup)
+
+     }
      
      return () => window.removeEventListener('keyup', handleKeyup)
      
-    }, [handleKeyup])
+    }, [handleKeyup, isCorrect])
 
-    useEffect(() => {
-      console.log(guesses, turn, isCorrect)
-    }, [guesses, turn, isCorrect])
+   
   return (
     <div>
     <div>solution - {solution}</div>
     <div>current guess - {currentGuess}</div>
     <Grid guesses={guesses} currentGuess={currentGuess}  turn={turn}/>
+    {showModal && <Modal isCorrect={isCorrect} turn={turn} solution= {solution} />}
     </div>
   )
 }
